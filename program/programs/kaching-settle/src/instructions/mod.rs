453eb4e4cbd72af20c2af5077fa924b154c7def8
cutlerjay109 <cutlerjay109@gmail.com>
2026-07-11 10:24:46 +0000

    Fix mod.rs: export void_market and refund

--- program/programs/kaching-settle/src/instructions/mod.rs
+++ program/programs/kaching-settle/src/instructions/mod.rs
@@ -3,9 +3,13 @@ pub mod deposit;
 pub mod lock_market;
 pub mod settle;
 pub mod claim;
+pub mod void_market;
+pub mod refund;
 
 pub use create_market::*;
 pub use deposit::*;
 pub use lock_market::*;
 pub use settle::*;
 pub use claim::*;
+pub use void_market::*;
+pub use refund::*;
